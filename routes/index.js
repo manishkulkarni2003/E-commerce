70df0cc62cff931b4241a46b168e68b0050c81e8
manishkulkarni2003 <mk3304939@gmail.com>
2024-10-08 23:12:02 +0530

    added cart functionality

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,41 @@ const express = require("express")
 const router = express.Router()
 const isLoggedin = require("../middlewares/isLoggedin")
 const productModel = require("../models/product.model")
+const userModel = require("../models/user.model")
 
 router.get("/", (req, res) => {
-    res.render("index", { error: '' })
+    res.render("index", { error: '', loggedin: false })
 })
 
 router.get("/shop", isLoggedin, async function (req, res) {
     let products = await productModel.find();
-    res.render("shop", { products })
+    let success = req.flash("success")
+    res.render("shop", { products, success })
+
+})
+router.get("/cart", isLoggedin, async function (req, res) {
+    let user = await userModel.findOne({ email: req.user.email }).populate("cart")
+
+    let totalBill = 0;
+
+    user.cart.forEach(item => {
+        const itemBill = (Number(item.price) + 20) - Number(item.discount)
+        totalBill = itemBill;
+    })
+
+
+    res.render("cart", { user, bill: totalBill });
+
+
+})
+
+router.get("/addtocart/:productid", isLoggedin, async function (req, res) {
+    let user = await userModel.findOne({ email: req.user.email })
+    user.cart.push(req.params.productid);
+    await user.save();
+    req.flash("success", "Added to Cart")
+    res.redirect("/shop")
+
 
 })
 
